a5ba86a111d874c564204db67278659b3cee6914
Raman <Raman_Trunin@epam.com>
2021-05-26 12:39:00 +0300

    fix

--- deps/settings.py
+++ deps/settings.py
@@ -12,12 +12,6 @@ POSTGRES_DB = os.getenv('POSTGRES_DB')
 SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db/{POSTGRES_DB}"
 
 
-SECRET_KEY = "45e76c0a8ca3d8e14ea260c0d9f05da92d1d6c961f78b5db567ebebd3a712510"
-
-ALGORITHM = "HS256"
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 LOGGER = {
     "path": str(Path(BASE_DIR / 'custom_logging/access.log')),
     "level": "info",
